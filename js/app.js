c1dda0c6ad010b313753ffa5d31d0fdac91f3f9e
bruno.alarcon <alarconbruno14@gmail.com>
2023-12-02 15:34:26 -0300

    agrego ejercicio 3 terminado

---
+++ js/app.js
@@ -0,0 +1,30 @@
+function actualizarReloj() {
+    const fecha = document.getElementById('fecha');
+  const reloj = document.getElementById("reloj");
+  const fechaActual = new Date();
+  let hora = fechaActual.getHours();
+  let minutos = fechaActual.getMinutes();
+  let segundos = fechaActual.getSeconds();
+  let periodo = "AM";
+
+  if (hora === 0) {
+    hora = 12;
+  } else if (hora > 12) {
+    periodo = "PM";
+    hora = hora - 12;
+  }
+
+  const horaCompleta = `${hora}:${minutos}:${segundos} ${periodo}`;
+
+  if(fecha){
+    fecha.innerText =  `${fechaActual.getDate()} / ${fechaActual.getMonth()} / ${fechaActual.getFullYear()}`;
+  }
+  if (reloj) {
+    reloj.innerText = horaCompleta;
+  }
+
+
+  setTimeout(actualizarReloj, 1000);
+}
+
+actualizarReloj();
